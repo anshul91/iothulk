0321f695a8ceda36d18be1c09a79b288158fd7d9
anshulpareek <anshulpareek91@gmail.com>
2018-06-11 23:48:42 +0530

    added device module

--- application/views/admin/device/index.php
+++ application/views/admin/device/index.php
@@ -86,7 +86,10 @@
                                             <div class="col-md-6">
                                                 <div class="form-group">
                                                     <label class="control-label">Device Type</label>
-                                            <input type="text" class="form-control" placeholder="Device Type"name="device_type" id="device_type">
+                                                        <select class="form-control custom-select">
+                                                            <option value="1">Single Value (sensor)</option>
+                                                            <option value="2">GPS (LAT. LONG.)</option>
+                                                        </select>
                                                 </div>
                                             </div>
                                             <!--/span-->
@@ -95,80 +98,33 @@
                                         <div class="row">
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <label class="control-label">Category</label>
-                                                    <select class="form-control custom-select" data-placeholder="Choose a Category" tabindex="1">
-                                                        <option value="Category 1">Category 1</option>
-                                                        <option value="Category 2">Category 2</option>
-                                                        <option value="Category 3">Category 5</option>
-                                                        <option value="Category 4">Category 4</option>
-                                                    </select>
+                                                    <label class="control-label">Sensor Name</label>
+                                                        <input type="text" class="form-control" placeholder="Sensor Name"name="device_type" id="device_type">
                                                 </div>
                                             </div>
                                             <!--/span-->
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <label class="control-label">Membership</label>
-                                                    <div class="form-check">
-                                                        <label class="custom-control custom-radio">
-                                                            <input id="radio1" name="radio" type="radio" checked="" class="custom-control-input">
-                                                            <span class="custom-control-indicator"></span>
-                                                            <span class="custom-control-description">Free</span>
-                                                        </label>
-                                                        <label class="custom-control custom-radio">
-                                                            <input id="radio2" name="radio" type="radio" class="custom-control-input">
-                                                            <span class="custom-control-indicator"></span>
-                                                            <span class="custom-control-description">Paid</span>
-                                                        </label>
-                                                    </div>
+                                                    <label class="control-label">Purpose</label>
+                                                    <textarea class="form-control" name="purpose" id="purpose"></textarea>
                                                 </div>
                                             </div>
                                             <!--/span-->
                                         </div>
                                         <!--/row-->
-                                        <h3 class="box-title m-t-40">Address</h3>
-                                        <hr>
-                                        <div class="row">
-                                            <div class="col-md-12 ">
-                                                <div class="form-group">
-                                                    <label>Street</label>
-                                                    <input type="text" class="form-control">
-                                                </div>
-                                            </div>
-                                        </div>
+                                            <!--/span-->
                                         <div class="row">
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <label>City</label>
-                                                    <input type="text" class="form-control">
+                                                    <label class="control-label">Description Name</label>
+                                                        <input type="text" class="form-control" placeholder="Sensor Name"name="device_type" id="device_type">
                                                 </div>
                                             </div>
                                             <!--/span-->
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <label>State</label>
-                                                    <input type="text" class="form-control">
-                                                </div>
-                                            </div>
-                                            <!--/span-->
-                                        </div>
-                                        <!--/row-->
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Post Code</label>
-                                                    <input type="text" class="form-control">
-                                                </div>
-                                            </div>
-                                            <!--/span-->
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Country</label>
-                                                    <select class="form-control custom-select">
-                                                        <option>--Select your Country--</option>
-                                                        <option>India</option>
-                                                        <option>Sri Lanka</option>
-                                                        <option>USA</option>
-                                                    </select>
+                                                    <label class="control-label">Purpose</label>
+                                                    <textarea class="form-control" name="purpose" id="purpose"></textarea>
                                                 </div>
                                             </div>
                                             <!--/span-->
